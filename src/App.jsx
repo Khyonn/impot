4c18c596cb488380badc28a36b0ca139d413753b
Khyonn <nathanael.morel@outlook.fr>
2024-02-09 23:39:51 +0100

    improve ui

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,113 @@ function App() {
 
   /** @type {HTMLFormElement}  */
   let form;
+  /** @type {HTMLDivElement}  */
+  let resultSection
   const onSubmit = () => {
     const data = Object.fromEntries(new FormData(form).entries());
     setResult(getCalculImpots(...tranches())(Number(data.annuel_brut), Number(data.nombre_de_mois)));
+    resultSection.scrollIntoView()
   };
 
   return (
-    <div class="p-4 flex flex-wrap gap-4">
-      <section class="grid gap-2">
+    <>
+      <div class="whole-screen-height bg-green-50 flex justify-center items-center">
+        <section class="bg-white shadow-lg rounded-lg w-80 p-4 grid gap-6">
+          <h2 class="text-xl font-semibold text-slate-800">Mon salaire après impôts</h2>
+          <form
+            class="grid sm:grid-cols-2 gap-4"
+            ref={form}
+            onSubmit={(event) => {
+              event.preventDefault();
+              onSubmit(event);
+            }}
+          >
+            <div class="sm:col-span-2 grid gap-1 items-center grid-cols-subgrid">
+              <label for="">Annuel brut</label>
+              <input
+                id="annuel_brut"
+                name="annuel_brut"
+                required
+                class="input"
+                type="number"
+                min="1"
+                placeholder="35000"
+              />
+            </div>
+            <div class="sm:col-span-2 grid gap-1 items-center grid-cols-subgrid">
+              <label for="">Nombre de mois</label>
+              <input
+                id="nombre_de_mois"
+                name="nombre_de_mois"
+                required
+                class="input"
+                type="number"
+                value="12"
+                min="12"
+                max="16"
+              />
+            </div>
+            <div class="sm:col-span-2 flex justify-end">
+              <button
+                class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 active:bg-green-700 active:scale-95 transition"
+                type="submit"
+              >
+                Calculer
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+      {result() && (
+        <div ref={resultSection} class="whole-screen-height bg-green-100 flex justify-center items-center">
+          <section class="bg-white rounded-lg shadow-lg w-96 p-4 grid gap-4">
+            <h2 class="sr-only">Résultats</h2>
+            <p class="text-justify">
+              Pour un salaire annuel brut de{" "}
+              <strong>
+                <Amount>{result().annuelBrut}</Amount>
+              </strong>
+              , on considèrera pour la suite que vous touchez{" "}
+              <strong>
+                <Amount>{result().annuelNet}</Amount> net
+              </strong>{" "}
+              (les différentes taxes et cotisations s'élevant à environ {(TAXE_BRUT_NET * 100).toFixed(2)}%)
+            </p>
+            <div class="grid gap-2">
+              Vous payez donc :
+              <ol class="list-disc pl-10">
+                <For each={result().montantParTranche}>
+                  {([montant, pourcentage]) => (
+                    <li>
+                      <Amount>{montant}</Amount> à un taux de {pourcentage}% (
+                      <Amount>{(montant * pourcentage) / 100}</Amount>)
+                    </li>
+                  )}
+                </For>
+              </ol>
+              <p class="text-justify">
+                Le montant total de votre impôt est de{" "}
+                <strong>
+                  <Amount>{result().impotTotal}</Amount>
+                </strong>{" "}
+                ce qui représente <strong>{result().pourcentageDImpotSurLAnnuelNet.toFixed(2)}%</strong> de votre annuel
+                net
+              </p>
+            </div>
+            <p class="text-justify">
+              Sur {result().mois} mois, si vous appliquez ce pourcentage sur chaque mois, vous touchez{" "}
+              <span class="text-green-600">
+                <strong>
+                  <Amount>{result().mensuelNetApresImpot}</Amount>
+                </strong>
+                / mois net après impôt
+              </span>
+            </p>
+          </section>
+        </div>
+      )}
+
+      <section class="">
         <h2 class="text-xl">Tranches d'imposition</h2>
         <ul class="list-disc pl-10">
           <For each={tranches()}>
@@ -46,96 +145,7 @@ function App() {
           </a>
         </em>
       </section>
-      <section class="w-80 grid gap-2">
-        <h2 class="text-xl">Calculer mes impôts</h2>
-        <form
-          class="grid grid-cols-2 gap-2"
-          ref={form}
-          onSubmit={(event) => {
-            event.preventDefault();
-            onSubmit(event);
-          }}
-        >
-          <div class="col-span-2 grid items-center grid-cols-subgrid">
-            <label for="">Annuel brut</label>
-            <input
-              id="annuel_brut"
-              name="annuel_brut"
-              required
-              class="input"
-              type="number"
-              min="1"
-              placeholder="35000"
-            />
-          </div>
-          <div class="col-span-2 grid items-center grid-cols-subgrid">
-            <label for="">Nombre de mois</label>
-            <input
-              id="nombre_de_mois"
-              name="nombre_de_mois"
-              required
-              class="input"
-              type="number"
-              value="12"
-              min="12"
-              max="16"
-            />
-          </div>
-          <div class="col-span-2 flex justify-end">
-            <button
-              class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 active:bg-blue-700 active:scale-95 transition"
-              type="submit"
-            >
-              Calculer
-            </button>
-          </div>
-        </form>
-      </section>
-      {result() && (
-        <section>
-          <hr />
-          <p>
-            Pour un salaire annuel brut de{" "}
-            <strong>
-              <Amount>{result().annuelBrut}</Amount>
-            </strong>
-            , on considèrera pour la suite que vous touchez{" "}
-            <strong>
-              <Amount>{result().annuelNet}</Amount> net
-            </strong>{" "}
-            (les différentes taxes s'élevant à environ {(TAXE_BRUT_NET * 100).toFixed(2)}%)
-          </p>
-          <div>
-            Vous payez donc :
-            <ol class="list-disc pl-10">
-              <For each={result().montantParTranche}>
-                {([montant, pourcentage]) => (
-                  <li>
-                    <Amount>{montant}</Amount> à un taux de {pourcentage}% (
-                    <Amount>{(montant * pourcentage) / 100}</Amount>)
-                  </li>
-                )}
-              </For>
-            </ol>
-            <p>
-              Le montant total de votre impôt est de{" "}
-              <strong>
-                <Amount>{result().impotTotal}</Amount>
-              </strong>{" "}
-              ce qui représente <strong>{result().pourcentageDImpotSurLAnnuelNet.toFixed(2)}%</strong> de votre annuel
-              net
-            </p>
-            <p>
-              Sur {result().mois} mois, si vous appliquez ce pourcentage sur chaque mois, vous touchez{" "}
-              <strong>
-                <Amount>{result().mensuelNetApresImpot}</Amount>
-              </strong>
-              /mois net après impôt
-            </p>
-          </div>
-        </section>
-      )}
-    </div>
+    </>
   );
 }
 
