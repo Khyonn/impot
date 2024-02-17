1f25865c700dd500b846d396d6e07267e5c2d5df
Khyonn <nathanael.morel@outlook.fr>
2024-02-17 19:30:42 +0100

    Ajout du statut

--- src/components/form/Specific.tsx
+++ src/components/form/Specific.tsx
@@ -1,21 +1,33 @@
 import { useSearchParams } from "~/context/SearchParams";
 import calculerImpots, { DEFAULT } from "~/rules/impots";
 import type { FormProps } from "./types";
+import { onCleanup } from "solid-js";
 
 export default function SpecificForm(props: FormProps) {
   const [, setParams] = useSearchParams();
 
+  let form: HTMLFormElement | undefined;
+  onCleanup(() => {
+    setParams(
+      Object.fromEntries(
+        Object.keys(Object.fromEntries(new FormData(form!)) as Record<string, string>).map((key) => [key, undefined])
+      )
+    );
+  });
+
   return (
     <form
-      class="w-full sm:w-96 grid gap-4 grid-cols-2"
+      ref={form}
+      class="w-60 sm:w-96 grid gap-4 sm:gap-2 sm:grid-cols-2"
       onSubmit={(event) => {
         event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        setParams(Object.fromEntries(new FormData(event.currentTarget)) as Record<string, string>);
+        const formData = Object.fromEntries(new FormData(event.currentTarget)) as Record<string, string>;
+        setParams(formData);
         props.onSubmit(
           calculerImpots({
-            annuelBrut: Number(formData.get("annuel_brut")),
-            nombreDeMois: Math.trunc(Number(formData.get("nombre_de_mois"))),
+            annuelBrut: Number(formData.annuel_brut),
+            nombreDeMois: Math.trunc(Number(formData.nombre_de_mois)),
+            pourcentageCotisationsBrutNet: Math.trunc(Number(formData.pourcentage_cotisations_brut_net)),
           })
         );
       }}
@@ -36,28 +48,20 @@ export default function SpecificForm(props: FormProps) {
           max="16"
         />
       </div>
-      {import.meta.env.DEV && (
-        <>
-          <div class="grid gap-1">
-            <label for="pourcentage_cotisations_brut_net">Statut</label>
-            <select
-              id="pourcentage_cotisations_brut_net"
-              name="pourcentage_cotisations_brut_net"
-              class="input"
-              required
-            >
-              <option value="22">Non cadre</option>
-              <option value="25" selected>
-                Cadre
-              </option>
-              <option value="11">Fonction publique</option>
-              <option value="45">Profession libérale</option>
-              <option value="51">Portage salarial</option>
-            </select>
-          </div>
-        </>
-      )}
-      <div class="col-span-2 flex justify-end">
+      <div class="grid gap-1">
+        <label for="pourcentage_cotisations_brut_net">Statut</label>
+        <select id="pourcentage_cotisations_brut_net" name="pourcentage_cotisations_brut_net" class="input" required>
+          <option value="22">Non cadre</option>
+          <option value="25" selected>
+            Cadre
+          </option>
+          <option value="11">Fonction publique</option>
+          <option value="45">Profession libérale</option>
+          <option value="51">Portage salarial</option>
+        </select>
+      </div>
+      {import.meta.env.DEV && <></>}
+      <div class="pt-2 sm:pt-0 sm:col-span-2 flex justify-end">
         <button
           class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 active:bg-green-700 active:scale-95 transition"
           type="submit"
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, from, on } from "solid-js";
+import { createEffect, createSignal, on } from "solid-js";
 import { useSearchParams } from "~/context/SearchParams";
 import { doTransition } from "~/helpers/transitions-api";
 
@@ -9,8 +9,8 @@ import type { FormProps } from "./types";
 import "./style.css";
 
 export default function ImpotsForm(props: FormProps) {
-  const [params, setParams] = useSearchParams<{ formType: string }>();
-  const getIsSpecificFromParams = () => params.formType === "specific";
+  const [params, setParams] = useSearchParams<{ form_type: string }>();
+  const getIsSpecificFromParams = () => params.form_type === "specific";
   const [isSpecific, setIsSpecific] = createSignal(getIsSpecificFromParams());
 
   createEffect(
@@ -37,7 +37,7 @@ export default function ImpotsForm(props: FormProps) {
         <button
           class="italic text-slate-400 transition-colors hover:text-slate-500 active:text-slate-600 text-xs relative isolate before:absolute before:-z-10 before:inset-0 before:border-b before:border-b-current before:origin-left before:transition-transform before:scale-x-0 hover:before:scale-x-100"
           onClick={() => {
-            setParams({ formType: isSpecific() ? undefined : "specific" });
+            setParams({ form_type: isSpecific() ? undefined : "specific" });
           }}
         >
           Accéder au formulaire {isSpecific() ? "simplifié" : "complet"}
